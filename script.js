106ac08c6b9b545b944b0ebea49e5f65cf734288
Andrius M <andriusmurn@gmail.com>
2022-10-01 14:13:04 +0300

    js comment delete

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
 console.log("Script loaded nav bar");
-// var menu1 = new Image();
-// menu1.src = "assets/shared/icon-hamburger.svg";
-// var menu2 = new Image();
-// menu2.src = "assets/shared/icon-close.svg";
 
 var navLinks = document.getElementById("nav-links");
-// var hamMenu = document.getElementById("toggle-ham-menu");
-
 var homeNavBtn = document.getElementById("home-nav-btn");
 var destNavBtn = document.getElementById("dest-nav-btn");
 var crewNavBtn = document.getElementById("crew-nav-btn");
 var techNavBtn = document.getElementById("tech-nav-btn");
 
-// var hamMenuIcon = document.getElementById("toggle-ham-menu").appendChild(menu1);
-
-// function imgClick() {
-//   if (hamMenuIcon.src.match("assets/shared/icon-hamburger.svg")) {
-//     hamMenuIcon.src = "assets/shared/icon-close.svg";
-//     navLinks.classList.add("navbar-links-active");
-//   } else {
-//     hamMenuIcon.src = "assets/shared/icon-hamburger.svg";
-//     navLinks.classList.remove("navbar-links-active");
-//   }
-// }
 homeNavBtn.classList.add("nav-btn-active");
 if (window.location.href.indexOf("index") != -1) {
   homeNavBtn.classList.add("nav-btn-active");
