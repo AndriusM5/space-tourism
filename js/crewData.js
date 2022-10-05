36c65aceaf821edb962c546aa9bd70f901320bce
Andrius M <andriusmurn@gmail.com>
2022-10-05 18:03:01 +0300

    crew page w/o js

---
+++ js/crewData.js
@@ -0,0 +1,38 @@
+import myJson from "../data.json" assert { type: "json" };
+
+// destination page
+
+console.log("Script loaded destination page");
+
+var radioBtn1 = document.getElementById("radio-one");
+var radioBtn2 = document.getElementById("radio-two");
+var radioBtn3 = document.getElementById("radio-three");
+var radioBtn4 = document.getElementById("radio-four");
+
+var role = document.getElementById("crewRole");
+var crewName = document.getElementById("crewName");
+var crewBio = document.getElementById("crewBio");
+var crewImg = document.getElementById("crewImg");
+
+var radioForm = document.getElementById("radioForm");
+
+function crewData(crewId) {
+  crewImg.src = myJson.crew[crewId].images.webp;
+  crewName.innerHTML = myJson.crew[crewId].name;
+  crewBio.innerHTML = myJson.crew[crewId].bio;
+  role.innerHTML = myJson.crew[crewId].role;
+}
+
+
+window.addEventListener("load", crewFunc);
+function crewFunc() {
+  if ((radioBtn1.checked == true)) {
+    crewData(0);
+  } else if ((radioBtn2.checked == true)) {
+    crewData(1);
+  } else if ((radioBtn3.checked == true)) {
+    crewData(2);
+  } else if ((radioBtn4.checked == true)) {
+    crewData(3);
+  }
+}
